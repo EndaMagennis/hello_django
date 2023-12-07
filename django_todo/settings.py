69aae37b1a1c1600ff2b4a5ff2a3092951497c05
EndaMagennis <>
2023-12-07 13:02:20 +0000

    Add full CRUD funtionality

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -21,7 +21,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-iic4!n##m)*z(*ki=#adzy_=wa19r)7gogo&sdfqgd$s@=!+d('  # noqa
+if os.path.isfile('env.py'):
+    import env
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
